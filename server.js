14c606cb54fbcf609012adc6ad8357311db65aaa
Jason Loomis <jason@jrloomis.com>
2019-11-08 13:34:27 -0500

    routes imported

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const projectRouter = require("./routes/projectRouter");
+const resourceRouter = require("./routes/resourceRouter");
+const taskRouter = require("./routes/taskRouter");
+
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
+
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
+server.use("/api/tasks", taskRouter);
 
 // * sanity
 server.get("/", (req, res) =>
