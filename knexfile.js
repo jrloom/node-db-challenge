99490d225ef9237239ed5b8f612813bc9f8da93b
Jason Loomis <jason@jrloomis.com>
2019-11-08 11:44:11 -0500

    foreign keys on

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,18 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./data/projects.sqlite3"
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: "./data/migrations"
     },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done)
     }
   }
-
 };
